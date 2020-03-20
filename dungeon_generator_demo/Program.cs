1b0c61280ed756a548cba93c9cf8bba28c3f0255
Felix <purodev@protonmail.com>
2020-03-20 18:04:04 +0700

    Added demo project.

--- dungeon_generator_demo/Program.cs
+++ dungeon_generator_demo/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace dungeon_generator {
+namespace dungeon_generator_demo {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
